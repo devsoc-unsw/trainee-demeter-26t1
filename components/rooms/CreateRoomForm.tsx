b3d777befb1bc42a9fca9874ef9fc192fd28b498
Yuva Naga Saicharan Potharaju <z5687086@ad.unsw.edu.au>
2026-05-18 14:47:12 +1000

    Created the form and added categories. And made a box to start on room code

--- components/rooms/CreateRoomForm.tsx
+++ components/rooms/CreateRoomForm.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 
 type CreateRoomFormData = {
   meetingName: string;
+  category: string;
   date: string;
   description: string;
 };
@@ -34,61 +35,97 @@ const router = useRouter();
   return (
     <div className="ml-5">
       
-      {/* HEADER */}
-      <h1 className="mt-10 mb-2 text-5xl font-bold text-gray-900 dark:text-white">
-        Create Your Meetup {user?.fname}
-      </h1>
+        {/* HEADER */}
+        <h1 className="mt-10 mb-2 text-5xl font-bold text-gray-900 dark:text-white">
+            Create Your Meetup {user?.fname}
+        </h1>
 
-      <p className="ml-1 text-lg text-gray-600 dark:text-gray-400 max-w-xl">
-        Take charge of your meetup and choose a place where meaningful
-        connection happen. Create a room and invite your friends. 
-      </p>
+        <p className="ml-1 text-lg text-gray-600 dark:text-gray-400 max-w-xl">
+            Take charge of your meetup and choose a place where meaningful
+            connection happen. Create a room and invite your friends. 
+        </p>
+        <div className="mt-15 flex gap-55">
+            { /* FORM BOX */}
+            <div className="mt-15 w-full max-w-3xl rounded-xl bg-gray-200/75 dark:bg-gray-800/75 p-6 backdrop-blur-sm">
 
-      {/* FORM BOX */}
-      <div className="mt-15 w-full max-w-3xl rounded-xl bg-gray-200/75 dark:bg-gray-800/75 p-6 backdrop-blur-sm">
+                <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
 
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+                    {/* Meeting Name */}
+                    <InputField
+                        label="Meeting Name"
+                        name="meetingName"
+                        placeholder="e.g. DevSoc TP hangout"
+                        register={register}
+                        error={errors.meetingName}
+                    />
+                    <div className="mt-15 flex gap-15">
+                        {/* Date */}
+                        <InputField className=" min-w-xs"
+                            label="Date"
+                            name="date"
+                            type="date"
+                            placeholder=""
+                            register={register}
+                            error={errors.date}
+                        />
 
-          {/* Meeting Name */}
-          <InputField
-            label="Meeting Name"
-            name="meetingName"
-            placeholder="e.g. DevSoc TP hangout"
-            register={register}
-            error={errors.meetingName}
-          />
+                        <div className=" min-w-xs">
+                            <label className="mb-2 block font-medium text-gray-900 dark:text-white">
+                                Category
+                            </label>
 
-          {/* Date */}
-          <InputField
-            label="Date"
-            name="date"
-            type="date"
-            placeholder=""
-            register={register}
-            error={errors.date}
-          />
+                            <select
+                                {...register("category")}
+                                className="w-full rounded-xl border-2 border-gray-300 dark:border-gray-700 bg-white dark:bg-[#0a0a0a] p-3 text-lg text-gray-900 dark:text-gray-100 outline-none focus:border-cyan-500"
+                            >
+                                <option value="">Select a category</option>
+                                <option value="food">Food</option>
+                                <option value="study">Study</option>
+                                <option value="sports">Sports</option>
+                                <option value="nightlife">Nightlife</option>
+                            </select>
+                        </div>
+                    </div>
+                    {/* Optional description */}
+                    <div className="min-w-xs">
+                        <TextArea 
+                            label=""
+                            name="description"
+                            placeholder="Add a description of the Room (optional)"
+                            rows={3}
+                            register={register}
+                            error={errors.description}
+                        />
+                    </div>
 
-          {/* Optional description */}
-          <TextArea 
-            label=""
-            name="description"
-            placeholder="Add a description of the Room (optional)"
-            rows={6}
-            register={register}
-            error={errors.description}
-          />
+                    {/* Button */}
+                    <button
+                        type="submit"
+                        disabled={isCreating}
+                        className="w-full rounded-xl bg-cyan-600 px-4 py-3 font-medium text-white transition hover:bg-cyan-500 disabled:opacity-70"
+                    >
+                        {isCreating ? "Creating..." : "Create Room"}
+                    </button>
+              
+                </form>
+                
+            </div>
+            <div className="mt-15 w-full max-w-3xl rounded-xl bg-gray-200/75 dark:bg-gray-800/75 p-6 backdrop-blur-sm">
 
-          {/* Button */}
-          <button
-            type="submit"
-            disabled={isCreating}
-            className="w-full rounded-xl bg-cyan-600 px-4 py-3 font-medium text-white transition hover:bg-cyan-500 disabled:opacity-70"
-          >
-            {isCreating ? "Creating..." : "Create Room"}
-          </button>
 
-        </form>
-      </div>
+
+
+
+
+
+
+
+
+            </div>
+        </div>
+
+
+        
     </div>
   );
 }
